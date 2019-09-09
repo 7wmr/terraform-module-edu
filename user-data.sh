19d6722782278376b9f3b8cbde07876afbd1afd3
Richard Neave <neaverds@icloud.com>
2019-09-09 15:09:32 +0100

    USER DATA
    - update html formatting.

--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
 cat <<EOF > index.html
-<p><b>Host name:</b> `hostname`</p>
-<br>
-<p><b>Host info:</b> `uname -a`</p>
-<br>
+<p><b>Hostname:</b> `hostname`</p>
+<p><b>System:</b> `uname -a`</p>
 EOF
 
 nohup busybox httpd -f -p "${server_port}" & 
