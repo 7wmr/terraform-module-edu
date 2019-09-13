c75f0eeb17f407bb42968e992b229b3cf04cd8ff
Richard Neave <neaverds@icloud.com>
2019-09-13 11:55:32 +0100

    USER DATA
    - update formatting for script

--- user-data.sh
+++ user-data.sh
@@ -2,7 +2,7 @@
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null \
 && git clone git@github.com:7wmr/terraform-mockapp-edu.git \
-&& ./terraform-mockapp-edu/setup.sh \
-&& ./terraform-mockapp-edu/run.sh
+&& sh terraform-mockapp-edu/setup.sh \
+&& sh terraform-mockapp-edu/run.sh
 
 
