cfbc783a46594d5fcc546061396d6e7cd102b8af
Richard Neave <neaverds@icloud.com>
2019-09-13 12:07:06 +0100

    GIT CLONE
    - use https rather than ssh for git clone

--- user-data.sh
+++ user-data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
-ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null \
-&& git clone git@github.com:7wmr/terraform-mockapp-edu.git \
+git clone https://github.com/7wmr/terraform-mockapp-edu.git \
 && sh terraform-mockapp-edu/setup.sh \
 && sh terraform-mockapp-edu/run.sh
 
