8846d0dcf1099a252fd3bd4b26b818b9ff999cc4
Richard Neave <neaverds@icloud.com>
2019-09-13 13:16:21 +0100

    GOPATH
    - ensure go path is declared

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
-git clone https://github.com/7wmr/terraform-mockapp-edu.git \
+GOPATH="$HOME/go" \
+&& mkdir $GOPATH \
+&& git clone https://github.com/7wmr/terraform-mockapp-edu.git \
 && sh terraform-mockapp-edu/setup.sh \
 && sh terraform-mockapp-edu/run.sh
 
