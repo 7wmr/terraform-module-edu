e064c204e62c874b05c450c23f5e21e5d38a5062
Richard Neave <neaverds@icloud.com>
2019-09-13 17:37:37 +0100

    LOGGING
    - add logging to file

--- user-data.sh
+++ user-data.sh
@@ -3,7 +3,7 @@ set -e -x
 
 sudo yum -y install git \
 && git clone https://github.com/7wmr/terraform-mockapp-edu.git \
-&& sh terraform-mockapp-edu/setup.sh \
+&& sh terraform-mockapp-edu/setup.sh  2>&1 | tee mockapp-setup.log\
 && sh terraform-mockapp-edu/run.sh
 
 
