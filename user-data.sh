99ab7c63f81e0d66c7e376201a98b6512f62836c
Richard Neave <neaverds@icloud.com>
2019-09-13 11:54:15 +0100

    SSH KEY
    - ensure ssh key added to know hosts.

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
-git clone git@github.com:7wmr/terraform-mockapp-edu.git
-./terraform-mockapp-edu/setup.sh
-./terraform-mockapp-edu/run.sh
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null \
+&& git clone git@github.com:7wmr/terraform-mockapp-edu.git \
+&& ./terraform-mockapp-edu/setup.sh \
+&& ./terraform-mockapp-edu/run.sh
 
 
