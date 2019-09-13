dc23dffcf87d64303c4f06b5220d49b2259a69dc
Richard Neave <neaverds@icloud.com>
2019-09-13 19:24:30 +0100

    GOLANG
    - fix issue with running golang app

--- user-data.sh
+++ user-data.sh
@@ -4,7 +4,8 @@ set -e -x
 sudo yum -y install git \
 && cd /tmp/ \
 && git clone https://github.com/7wmr/terraform-mockapp-edu.git \
-&& sh terraform-mockapp-edu/setup.sh  2>&1 | tee /tmp/mockapp-setup.log\
-&& sh terraform-mockapp-edu/run.sh
+&& cd terraform-mockapp-edu \
+&& sh setup.sh  2>&1 | tee /tmp/mockapp-setup.log \
+&& nohup go run main.go &
 
 
