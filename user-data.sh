993c51f91e836c83fa17565067aac3ae95ce40c7
Richard Neave <neaverds@icloud.com>
2019-09-13 14:19:33 +0100

    GOLANG INSTALL
    - move code into app repo

--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
 set -x
 
-export GOPATH="$HOME/go" \
-&& mkdir $GOPATH \
-&& git clone https://github.com/7wmr/terraform-mockapp-edu.git \
+git clone https://github.com/7wmr/terraform-mockapp-edu.git \
 && sh terraform-mockapp-edu/setup.sh \
 && sh terraform-mockapp-edu/run.sh
 
