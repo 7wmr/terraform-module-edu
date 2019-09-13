9eb6e8e36fdfc5023164f20bf0a7b1dad6196cff
Richard Neave <neaverds@icloud.com>
2019-09-13 11:27:59 +0100

    MOCK APP
    - update user-data to clone and trigger mock app.

--- user-data.sh
+++ user-data.sh
@@ -1,10 +1,6 @@
 #!/bin/bash 
-cat <<EOF > index.html
-<p><b>Hostname:</b> `hostname`</p>
-<p><b>System:</b> `uname -a`</p>
-EOF
-
-nohup busybox httpd -f -p "${server_port}" & 
-EOF
+git clone git@github.com:7wmr/terraform-mockapp-edu.git
+./terraform-mockapp-edu/setup.sh
+./terraform-mockapp-edu/run.sh
 
 
