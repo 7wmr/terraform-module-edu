26daefddea343342191324edd025d71efbd3fe1d
Richard Neave <neaverds@icloud.com>
2019-09-13 13:30:50 +0100

    VARIABLE
    - export gopath variable

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-GOPATH="$HOME/go" \
+export GOPATH="$HOME/go" \
 && mkdir $GOPATH \
 && git clone https://github.com/7wmr/terraform-mockapp-edu.git \
 && sh terraform-mockapp-edu/setup.sh \
