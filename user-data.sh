145b2de48cc212ee7bee4281cf289f183ab4f8ad
Richard Neave <neaverds@icloud.com>
2019-09-13 19:05:55 +0100

    LOGGING
    - move files to /tmp dir

--- user-data.sh
+++ user-data.sh
@@ -2,8 +2,9 @@
 set -e -x
 
 sudo yum -y install git \
+&& cd /tmp/
 && git clone https://github.com/7wmr/terraform-mockapp-edu.git \
-&& sh terraform-mockapp-edu/setup.sh  2>&1 | tee mockapp-setup.log\
+&& sh terraform-mockapp-edu/setup.sh  2>&1 | tee /tmp/mockapp-setup.log\
 && sh terraform-mockapp-edu/run.sh
 
 
