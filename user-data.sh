aab49c76e6e8235d95bb0db04604859707b442b8
Richard Neave <neaverds@icloud.com>
2019-09-16 14:11:40 +0100

    HTML PAGE
    - add a root template page

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+cat <<EOF > index.tmpl
+<html>
+  <h1> {{ .title }}</h1>
+
+  <table>
+    <tr>
+      <th>Version</th>
+      <td>${app_version}</td>
+    </tr>
+  </table>
+
+</html>
+EOF
+
 curl -L -O "https://github.com/7wmr/terraform-mockapp-edu/releases/download/${app_version}/mockapp_linux_amd64" \
 && chmod +x ./mockapp_linux_amd64 \
 && nohup ./mockapp_linux_amd64 -port ${app_port} &
