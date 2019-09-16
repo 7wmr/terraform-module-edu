b355f9e733ddfa1d682beb9d88823df125394796
Richard Neave <neaverds@icloud.com>
2019-09-16 12:03:42 +0100

    USER DATA
    - fix user data binary download issue

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e -x
-
-curl -O "https://github.com/7wmr/terraform-mockapp-edu/releases/download/${app_version}/mockapp_linux_amd64" \
-&& nohup mockapp_linux_amd64 &
+curl -L -O "https://github.com/7wmr/terraform-mockapp-edu/releases/download/${app_version}/mockapp_linux_amd64" \
+&& chmod +x ./mockapp_linux_amd64 \
+&& nohup ./mockapp_linux_amd64 &
 
