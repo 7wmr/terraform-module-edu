8a98c43654c2e3b5374e6ee07c8e99be056e8aec
Richard Neave <neaverds@icloud.com>
2019-09-17 14:15:50 +0100

    RABBITMQ
    - add region variable

--- messaging/rabbitmq/variables.tf
+++ messaging/rabbitmq/variables.tf
@@ -1,3 +1,8 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to be used"
+}
+
 variable "cluster_name" {
   type        = string
   description = "Name of cluster to be created."
