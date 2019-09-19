b07b0241479f555143bd76e23a0c2ee818cedf82
Richard Neave <neaverds@icloud.com>
2019-09-19 13:48:24 +0100

    SVC WEB
    - rename application

--- svc/web/user-data.sh
+++ svc/web/user-data.sh
@@ -5,7 +5,7 @@ cat <<EOF > index.tmpl
 ${index_tmpl}
 EOF
 
-curl -L -O "https://github.com/7wmr/terraform-mockapp-edu/releases/download/${app_version}/mockapp_linux_amd64" \
-&& chmod +x ./mockapp_linux_amd64 \
-&& nohup ./mockapp_linux_amd64 --port ${app_port} --msg-endpoint '${rabbitmq_endpoint}' --msg-credentials '${rabbitmq_credentials}' &
+curl -L -O "https://github.com/7wmr/terraform-svc-web-edu/releases/download/${app_version}/svc_web_linux_amd64" \
+&& chmod +x ./svc_web_linux_amd64 \
+&& nohup ./svc_web_linux_amd64 --port ${app_port} --msg-endpoint '${rabbitmq_endpoint}' --msg-credentials '${rabbitmq_credentials}' &
 
